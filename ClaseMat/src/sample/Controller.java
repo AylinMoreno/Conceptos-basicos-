6e8dc0b3bb6589aaef0125b2dd6a4ab61b70542c
AylinMoreno <aylin.mireles2001@gmail.com>
2020-10-28 12:26:39 -0700

    el repaso o no se que
    me estrece jajaja como siempre

---
+++ ClaseMat/src/sample/Controller.java
@@ -0,0 +1,87 @@
+package sample;
+
+import Calculos.Calculos;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextField txtArreglo1, txtArreglo2;
+    @FXML
+    ListView lista1,lista2;
+    int []arreglo1=new int[5];
+    int [][]arreglo2= new int[2][4];
+    int cont=0;
+    int cont2=0;
+    int cont3=0;
+    public void insertar1(ActionEvent event) {
+        if (cont < 5) {
+            double dato =Double.parseDouble(txtArreglo1.getText());
+            arreglo1[cont] = (int) dato;
+            cont++;
+            txtArreglo1.setText("");
+            imprimirArreglo(arreglo1);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setContentText("Arreeglo lleno");
+            alert.show();
+        }
+    }
+
+    public void insertar2(ActionEvent event) {
+        if (cont2 < 2) {
+            double dato = Double.parseDouble(txtArreglo2.getText());
+            arreglo2[cont2][cont3] = (int) dato;
+            cont3++;
+
+            if(cont3 == 4 ){
+                cont3=0;
+                cont2 ++;
+            }
+            txtArreglo2.setText("");
+            imprimirArregloBidi(arreglo2);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setContentText("Arreeglo lleno");
+            alert.show();
+        }
+    }
+    public void procesar1(ActionEvent event) {
+         Calculos calculos=new Calculos(arreglo1,arreglo2);
+        double[] res = calculos.procesarCoseno();
+        for (double dato:res){
+            lista1.getItems().add(dato+"");
+        }
+    }
+
+    public void procesar2(ActionEvent event) {
+        Calculos calculos=new Calculos(arreglo1,arreglo2);
+        double[] res = calculos.procesarCoseno();
+        for (int x=0;x<res.length; x++) {
+            String columna ="";
+            for (int y = 0; y < res[x];y++) {
+                columna= columna+res[x][y]+",";
+            }
+            lista2.getItems().add(columna);
+        }
+    }
+    private void imprimirArreglo(int[] arr) {
+        for (int x = 0; x < arr.length; x++) {
+            System.out.println(arr[x]+",");
+
+        }
+        System.out.println("");
+    }
+    private void imprimirArregloBidi(int[][] arr) {
+        for(int x=0; x<arr.length;x++){
+            for(int y=0; y<arr[x].length;y++){
+                System.out.println(arr[x][y]+",");
+            }
+        }
+    }
+}
